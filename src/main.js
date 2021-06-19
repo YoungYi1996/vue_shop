e50d3c6f5393596e1c34a3834a4d3d11ed4ed0d3
Young <912624251@qq.com>
2021-06-19 19:56:17 +0800

    完成了用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 //导入字体图标
-import './assets/font_cgqtocrtnug/iconfont.css'
+import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 
@@ -13,6 +13,11 @@ Vue.config.productionTip = false
 
 //配置请求的根路径
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+axios.interceptors.request.use(config => {
+    // console.log(config);
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
+})
 Vue.prototype.$http = axios
 
 new Vue({
